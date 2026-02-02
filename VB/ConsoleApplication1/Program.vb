8a360caac38e91c157a374cd19acaae85f707a27
DevExpressExampleBot <fakeEmail>
2026-02-02 22:58:59 +0400

    Source auto update [skip ci]

--- VB/ConsoleApplication1/Program.vb
+++ VB/ConsoleApplication1/Program.vb
@@ -1,47 +1,37 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
-
 Imports DevExpress.Xpo
-
 Imports DevExpress.Xpo.DB
 Imports DevExpress.Data.Filtering
 
-
 Namespace ConsoleApplication1
 
-	Friend Class Program
+    Friend Class Program
 
-		Shared Sub Main(ByVal args() As String)
+        Shared Sub Main(ByVal args As String())
+            XpoDefault.DataLayer = XpoDefault.GetDataLayer("http://localhost:55777/Service1.svc", AutoCreateOption.DatabaseAndSchema)
+            XpoDefault.Session = Nothing
+            Using uow As UnitOfWork = New UnitOfWork()
+                If uow.FindObject(GetType(Customer), New BinaryOperator("ContactName", "Alex Smith", BinaryOperatorType.Equal)) Is Nothing Then
+                    Dim custAlex As Customer = New Customer(uow)
+                    custAlex.ContactName = "Alex Smith"
+                    custAlex.CompanyName = "DevExpress"
+                    custAlex.Save()
+                    Dim Tom As Customer = New Customer(uow)
+                    Tom.ContactName = "Tom Jensen"
+                    Tom.CompanyName = "ExpressIT"
+                    Tom.Save()
+                    uow.CommitChanges()
+                End If
 
-			XpoDefault.DataLayer = XpoDefault.GetDataLayer("http://localhost:55777/Service1.svc", AutoCreateOption.DatabaseAndSchema)
+                Using customers As XPCollection(Of Customer) = New XPCollection(Of Customer)(uow)
+                    For Each customer As Customer In customers
+                        Console.WriteLine("Company Name = {0}; ContactName = {1}", customer.CompanyName, customer.ContactName)
+                    Next
+                End Using
+            End Using
 
-			XpoDefault.Session = Nothing
-
-			Using uow As New UnitOfWork()
-				If uow.FindObject(GetType(Customer), New BinaryOperator("ContactName", "Alex Smith", BinaryOperatorType.Equal)) Is Nothing Then
-				Dim custAlex As New Customer(uow)
-				custAlex.ContactName = "Alex Smith"
-				custAlex.CompanyName = "DevExpress"
-				custAlex.Save()
-
-				Dim Tom As New Customer(uow)
-				Tom.ContactName = "Tom Jensen"
-				Tom.CompanyName = "ExpressIT"
-				Tom.Save()
-				uow.CommitChanges()
-				End If
-
-				Using customers As New XPCollection(Of Customer)(uow)
-
-					For Each customer As Customer In customers
-
-						Console.WriteLine("Company Name = {0}; ContactName = {1}", customer.CompanyName, customer.ContactName)
-					Next customer
-				End Using
-			End Using
-
-			Console.WriteLine("Press any key...")
-			Console.ReadKey()
-		End Sub
-	End Class
+            Console.WriteLine("Press any key...")
+            Console.ReadKey()
+        End Sub
+    End Class
 End Namespace
